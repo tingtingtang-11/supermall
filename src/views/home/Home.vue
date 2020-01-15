bd45eb42a5b19f79ea9d53db4cce3ec8ff7a34c0
tangtingting-11 <1149218521@qq.com>
2020-01-15 20:41:18 +0800

    增加featureView、修复一些滚动时样式的问题

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,6 +5,7 @@
     </nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
   </div>
 </template>
 
@@ -12,6 +13,7 @@
   import NavBar from "components/common/navbar/NavBar";
   import HomeSwiper from "./childComps/HomeSwiper";
   import RecommendView from "./childComps/RecommendView";
+  import FeatureView from "./childComps/FeatureView";
 
   import {getHomeMultiData} from "network/home";
 
@@ -20,7 +22,8 @@
     components: {
       NavBar,
       HomeSwiper,
-      RecommendView
+      RecommendView,
+      FeatureView
     },
     data(){
       return{
@@ -49,9 +52,21 @@
 </script>
 
 <style scoped>
+  #home{
+    padding-top: 44px;
+  }
+
     .home-nav{
-        background-color: var(--color-tint);
-        color: #ffffff;
+      background-color: var(--color-tint);
+      color: #ffffff;
+
+      /*把顶部nav固定住*/
+      position: fixed;
+      left: 0;
+      right: 0;
+      top: 0;
+    /*  nav又被轮播图盖住了*/
+      z-index: 9;
     }
 
 </style>
